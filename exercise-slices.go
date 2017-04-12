8631e3ea9d6ab6403ec50a49f67d0b5398eacf7a
Kenta Fukushima <dev.kentac55@gmail.com>
2017-04-12 23:39:46 +0900

    add exercise-slices.go

---
+++ exercise-slices.go
@@ -0,0 +1,18 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	res := make([][]uint8, dx)
+	for x := 0; x < dx; x++ {
+		res[x] = make([]uint8, dy)
+		for y := 0; y < dy; y++ {
+			res[x][y] = uint8(x ^ y)
+		}
+	}
+	return res
+}
+
+func main() {
+	pic.Show(Pic)
+}
