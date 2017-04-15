d298337ac39342dacbf1ca93557b4dd95182db0c
Kenta Fukushima <dev.kentac55@gmail.com>
2017-04-15 11:40:59 +0900

    reduce comments and fix functions

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -5,84 +5,47 @@ import (
 	"math"
 )
 
-// 計算(なんとfloat64には^が使えない)
-func getNext(input float64, target float64) float64 {
-	return input - (input * input - target) / (input * 2)
+func calcNextNum(input float64, target float64) float64 {
+	return input - (input*input-target)/(input*2)
 }
 
-// 終了判定
-func chkAcc(current float64, past1 float64, past2 float64) bool {
-	// 前回と同じもしくは前々回と同じ場合trueを返す
-	if current == past1 || current == past2 {
+func isFinished(current float64, past1stNum float64, past2ndNum float64) bool {
+	if current == past1stNum || current == past2ndNum {
 		return true
 	}
 	return false
 }
 
-// 指定のフォーマットでecho
-func echoMsg(count int, value float64, real bool) int {
-	imag := ""
-	if !real {
-		imag = "i"
-	}
-	fmt.Printf("%v回目の試行: \t%v%v\n", count, value, imag)
-	// todo: Unit?の返し方
-	return 0
-}
-
-func Sqrt(x float64, tryNum int) float64 {
-	var (
-		init = 1.0
-		real = true
-	)
+func Sqrt(x float64) float64 {
 
 	if x == 0 {
-		// 0の場合は正常な値が出ない
-		println(0)
+		fmt.Println(0)
 		return 0.0
 	} else if x < 0 {
-		// xが負の数のとき
-		x = -x
-		real = false
+		fmt.Println("E: complex")
+		return 0.0
 	}
 
-	// 手動計算
-	if tryNum > 0 {
-		next := init
-		for i := 1; i <= tryNum; i++ {
-			next = getNext(next, x)
-			echoMsg(i, next, real)
-		}
-		return next
-	}
-
-	// 自動計算
 	var (
-		next  = init
-		past1 = -1.0
-		past2 = -1.0
-		max   = 100
+		nextNum      = 1.0
+		past1stNum   = -1.0
+		past2ndNum   = -1.0
+		maxTryCounts = 100
 	)
 
-	// todo: なぜかこのprintlnが遅延する
-	// println("---自動計算ログ---")
-
-	// 念の為chkAccに漏れがあった場合に備えて試行回数上限を設ける
-	for i := 1; !chkAcc(next, past1, past2) && i <= max; i++ {
-		past2 = past1
-		past1 = next
-		next = getNext(next, x)
-		// 計算ログ出力
-		echoMsg(i, next, real)
-		if i == max {
-			println("試行上限に達した為停止します")
+	for i := 1; !isFinished(nextNum, past1stNum, past2ndNum) && i <= maxTryCounts; i++ {
+		past2ndNum = past1stNum
+		past1stNum = nextNum
+		nextNum = calcNextNum(nextNum, x)
+		if i == maxTryCounts {
+			fmt.Println("reached maxTryCounts.")
 		}
 	}
-	return next
+	return nextNum
 }
 
 func main() {
-	// Sqrt(2, 10)
-	println("自作関数の答え: ", Sqrt(2, 0))
-	println("組込関数の答え: ", math.Sqrt(2))
+	num := 2.0
+	fmt.Println("自作関数の答え: ", Sqrt(num))
+	fmt.Println("組込関数の答え: ", math.Sqrt(num))
 }
